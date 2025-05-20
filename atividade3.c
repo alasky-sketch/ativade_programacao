05083556fdb245f4f96bad673fb1be0d61b65669
alasky-sketch <133282543+alasky-sketch@users.noreply.github.com>
2025-05-20 20:01:03 -0300

    Add files via upload

---
+++ atividade3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{  
+    int dia;
+    printf("Digite um número (1-7): ");
+    scanf("%d", &dia);
+    switch(dia){
+        case 1:
+        printf("Domingo");
+        break;
+        case 2:
+        printf("Segunda-feira");
+        break;
+        case 3:
+        printf("Terça-feira");
+        break;
+        case 4:
+        printf("Quarta-feira");
+        break;
+        case 5:
+        printf("Quinta-feira");
+        break;
+        case 6:
+        printf("Sexta-feira");
+        break;
+        case 7:
+        printf("Sábado");
+        break;
+        default:
+        printf("Dia inválido");
+    }
+    
+    
+    return 0;
+}
